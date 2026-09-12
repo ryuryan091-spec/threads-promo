da1f7de543467e5e1f15abfac7610e009cfbcd97
ryuryan091-spec <ryuryan091@gmail.com>
2026-09-13 00:50:15 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@
 # ---------------------------------------------------------------------------
 # TODO(마스터 확인 필요): YouTube 채널 핸들을 실제 값으로 교체할 것.
 #   추측 입력 금지. 미교체 상태로 실행하면 main.py가 시작 시점에 중단한다.
-YOUTUBE_URL = "https://www.youtube.com/@REPLACE_ME"
+YOUTUBE_URL = "https://www.youtube.com/@tiger18272-y"
 X_URL = "https://x.com/tiger18272"
 
 YOUTUBE_URL_PLACEHOLDER = "@REPLACE_ME"
